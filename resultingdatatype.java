9825bc5182f75339b60a3d3b0d3af812b7155464
Naman Jain <103673538+NamanJain62@users.noreply.github.com>
2022-04-22 14:03:17 +0530

    Add files via upload

---
+++ resultingdatatype.java
@@ -0,0 +1,16 @@
+public class resultingdatatype {
+    public static void main(String[] args) {
+        int a=46+8;
+        short x=8;
+        int z=x+a;
+        System.out.println(z);
+
+        //Increment and decrement
+        int i =45;
+        System.out.println(i++);//firstly we assign i (45) then i increment(46)
+        System.out.println(i);
+        System.out.println(++i);//firstly increment i (47) then i assign (47)
+        System.out.println(i);
+
+    }
+}
